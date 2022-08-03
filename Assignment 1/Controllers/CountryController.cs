72a7011e642c84f06e4101721f8dcb5ec279b201
samreenst <74899669+samreenst@users.noreply.github.com>
2022-08-03 11:20:39 +0530

    Add files via upload

---
+++ Assignment 1/Controllers/CountryController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [RoutePrefix("api/country")]
+    public class CountryController : ApiController
+    {
+        static List<Country> countrylist = new List<Country>()
+        {
+            new Country{Id=1, CountryName="India",Capital="Delhi"},
+            new Country{Id=2, CountryName="Australia",Capital="Sydney"},
+            new Country{Id=3, CountryName="France",Capital="Paris"},
+            new Country{Id=4, CountryName="America",Capital="Washington DC"},
+        };
+        [HttpGet]
+        [Route("countrydetails")]
+
+        public IEnumerable<Country> Get()
+        {
+            return countrylist;
+        }
+
+        [HttpGet]
+        [Route("countrylist")]
+
+        public HttpResponseMessage GetCountryList()
+        {
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, countrylist);
+            return response;
+        }
+
+        [HttpGet]
+        [Route("p")]
+        public HttpResponseMessage GetP(int pid)
+        {
+            var country = countrylist.Find(x => x.Id == pid);
+
+            if (country == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound, pid);
+            }
+            return Request.CreateResponse(HttpStatusCode.OK, country);
+        }
+
+
+        //Post
+
+        public Country Post([FromBody] Country p)
+        {
+            countrylist.Add(p);
+            return p;
+        }
+
+        //put
+        public IEnumerable<Country> Put(int id, [FromBody] Country country)
+        {
+            countrylist[id - 1] = country;
+            return countrylist;
+        }
+
+        [HttpGet]
+        [Route("ById")]
+        public IHttpActionResult GetID(int pid)
+        {
+            var country = countrylist.Find(x => x.Id == pid);
+
+            if (country == null)
+            {
+                return NotFound();
+            }
+            return Ok(country);
+        }
+
+        [HttpGet]
+        [Route("CountryName")]
+        public IHttpActionResult GetName(int pid)
+        {
+            string country = countrylist.Where(x => x.Id == pid).SingleOrDefault()?.CountryName;
+
+            if (country == null)
+            {
+                return NotFound();
+            }
+            return Ok(country);
+            //return new MyResult(country, Request);
+
+
+        }
+    }
+}
---
+++ Assignment 1/Controllers/DemoController.cs
@@ -0,0 +1,59 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Net;
+//using System.Net.Http;
+//using System.Web.Http;
+//using WebAPI.Models;
+
+//namespace WebAPI.Controllers
+//{
+//    public class DemoController : ApiController
+//    {
+//        static List<string> mystrings = new List<string> { "Value0", "Value1", "Value2", "Value3" };
+
+//        //GET : api/Demo
+
+//        public IEnumerable<string> Get()
+//        {
+//            return mystrings;
+//        }
+
+//        //GET :api/Demo/2
+//        public string Get(int id)
+//        {
+//            return mystrings[id];
+//        }
+
+//        //POST 
+//        [HttpPost]
+//        //public IEnumerable<string> Post([FromUri] string val)
+//        //{
+
+//        //    mystrings.Add(val);
+//        //    return mystrings;
+//        //}
+
+//        public IEnumerable<string> Post([FromUri] sampleModel m)
+//        {
+//            mystrings.Add(m.name);
+//            return mystrings;
+//        }
+
+//        //Put
+
+//        public IEnumerable<string> Put(int id, [FromUri] string val)
+//        {
+//            mystrings[id - 1] = val;
+//            return mystrings;
+//        }
+
+//        //Delete
+
+//        public IEnumerable<string> Delete(int id)
+//        {
+//            mystrings.RemoveAt(id);
+//            return mystrings;
+//        }
+//    }
+//}
